4d2174b74af5d09f1dba154468fe81d2b81f1e53
Anthony Kieu <anthonykieu@gmail.com>
2021-02-13 16:19:32 -0500

    empty cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,29 +1,29 @@
 const APP_PREFIX = 'Budget-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-
+const DATA_CACHE_NAME = APP_PREFIX + "data"+ VERSION;
 const FILES_TO_CACHE = [
-    // "./",
-    "./js/idb.js",
-    "./index.html",
-    "./css/style.css",
-    "./js/index.js",
-    "./manifest.json",
-    "./icons/icon-72x72.png",
-    "./icons/icon-96x96.png",
-    "./icons/icon-128x128.png",
-    "./icons/icon-144x144.png",
-    "./icons/icon-152x152.png",
-    "./icons/icon-192x192.png",
-    "./icons/icon-384x384.png",
-    "./icons/icon-512x512.png"
+    "/",
+    "/js/idb.js",
+    "/index.html",
+    "/css/styles.css",
+    "/js/index.js",
+    "/manifest.json",
+    "/icons/icon-72x72.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-152x152.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-512x512.png"
 ];
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
+            return cache.addAll(FILES_TO_CACHE);
         })
     )
 })
@@ -46,18 +46,31 @@ self.addEventListener('activate', function (e) {
     );
 });
 self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {       // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-            // You can omit if/else for console.log & put one line below like this too.
-            // return request || fetch(e.request)
-        })
-    )
+    if (e.request.url.includes('/api/')) {
+        e.respondWith(
+            caches
+                .open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(e.request)
+                        .then(response => {
+                            console.log(response)
+                            // If the response was good, clone it and store it in the cache.
+                            if (response.status === 200) {
+                                cache.put(e.request.url, response.clone());
+                            }
+
+                            return response;
+                        })
+                        .catch(err => {
+                            console.error(err)
+                            // Network request failed, try to get it from the cache.
+                            return cache.match(e.request);
+                        });
+                })
+                .catch(err => console.log(err))
+        );
+
+        return;
+    }
+
 })
