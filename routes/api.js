5f450d068db76023572992c2291430f7bca1ddc6
Anthony Kieu <anthonykieu@gmail.com>
2021-02-14 16:27:23 -0500

    Removed express variable in api.js and modified start url in manifest.json

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
-const e = require("express");
 const Transaction = require("../models/transaction.js");
 
-router.post("/api/transaction", ({body}, res) => {
+router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -12,7 +11,7 @@ router.post("/api/transaction", ({body}, res) => {
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+router.post("/api/transaction/bulk", ({ body }, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -23,13 +22,11 @@ router.post("/api/transaction/bulk", ({body}, res) => {
 });
 
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
+  Transaction.find({}).sort({ date: -1 })
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
     .catch(err => {
-    
-      console.error(err);
       res.status(404).json(err);
     });
 });
